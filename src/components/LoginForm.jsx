6640fb27a543f5229c357b3dda8685ede7be121f
elduke717 <spacematcha@proton.me>
2023-07-03 10:24:22 -0400

    add LoginForm

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+
+function LoginForm({handleLogin}) {}) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onSubmit = (e) => {
+      e.preventDefault();
+      handleLogin({email, password});
+      setEmail('');
+      setPassword('');
+  }
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default LoginForm;
