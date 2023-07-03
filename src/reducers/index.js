afc2fe570d487c49ba35b6bdbde8222ed590d7ed
elduke717 <spacematcha@proton.me>
2023-07-02 20:14:13 -0400

    implement redux

---
+++ src/reducers/index.js
@@ -0,0 +1,16 @@
+const initialState = {
+    transactions: [],
+};
+
+const budgetReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'ADD_TRANSACTION': 
+            return { { ...state, transactions: [...state.transactions, action.payload]} };
+        case 'DELETE_TRANSACTION':
+            return { ...state, transactions: state.transactions.filter(transaction => transaction.id !== action.payload) };
+        default:
+            return state;
+    }
+};
+
+export default budgetReducer;
